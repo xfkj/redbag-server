c4b4b8f598c4df6ae89c5feaef61fbcbbf827343
xfkj <it@ffjy.org>
2018-01-29 18:28:52 +0800

    修改gitignore,去除机密信息

--- wx.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  //公众号
-  APP_ID: 'wx0691d24964171a10',
-  APP_SECRET: '607f57e342f947e9e428cb528dedbd38',
-  //微信支付号
-  MCH_ID: '1494533882', //微信支付商户号
-  KEY: 'C7AA06BC2675D6CBD979FC3875F8D64B',//商户密钥
-}
